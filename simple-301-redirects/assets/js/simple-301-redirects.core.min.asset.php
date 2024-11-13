38169bf49b4673a14c5d7c617412fd75be78c70b
Justin Gardner <justin@rhynorater.com>
2024-11-13 06:06:07 -0500

    2.0.11

--- simple-301-redirects/assets/js/simple-301-redirects.core.min.asset.php
+++ simple-301-redirects/assets/js/simple-301-redirects.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'caa194a52f36ba02eb10');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'b677fdc45aa0ed0a692d');
